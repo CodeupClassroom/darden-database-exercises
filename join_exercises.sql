8bd033f8c481bdfdd76432d71ad575023432ee60
RyanOrsinger <ryan.orsinger@gmail.com>
2020-07-23 17:31:19 -0500

    add solutions for join exercises

---
+++ join_exercises.sql
@@ -0,0 +1,94 @@
+
+
+-- Find the name of all departments currently managed by women.
+select e.first_name, e.last_name
+from employees e
+join dept_manager dm on dm.emp_no = e.emp_no
+join departments d on d.dept_no = dm.dept_no
+where dm.to_date > curdate()
+and gender = 'f';
+
+
+-- Find the current titles of employees currently working in the Customer Service department.
+select t.title, count(*)
+from titles as t 
+join dept_emp as de on de.emp_no = t.emp_no
+join departments d on de.dept_no = d.dept_no
+where de.to_date > curdate()
+and t.to_date > curdate()
+and d.dept_name = "Customer Service"
+group by t.title;
+
+
+
+
+-- Find the current salary of all current managers.
+-- dept_name, employee name, salary for current managers
+select dept_name, first_name, last_name, salary
+from employees
+join dept_manager using(emp_no)
+join departments using(dept_no)
+join salaries using(emp_no)
+where dept_manager.to_date > curdate()
+and salaries.to_date > curdate()
+order by dept_name;
+
+
+-- Find the number of employees in each department.
+select dept_no, dept_name, count(*)
+from departments
+join dept_emp using(dept_no)
+where dept_emp.to_date > curdate()
+group by dept_no;
+
+
+
+-- Which department has the highest average salary?
+select dept_name, avg(salary) as "Highest Avg Salary of All Departments"
+from salaries
+join dept_emp using(emp_no)
+join departments using(dept_no)
+where dept_emp.to_date > curdate()
+and salaries.to_date > curdate()
+group by dept_no
+order by avg(salary) desc
+limit 1;
+
+
+/* Who is the highest paid employee in the Marketing department?
++------------+-----------+
+| first_name | last_name |
++------------+-----------+
+| Akemi      | Warwick   | */
+select first_name, last_name
+from employees
+join dept_emp using(emp_no)
+join departments using(dept_no)
+join salaries using(emp_no)
+where dept_name = "Marketing"
+order by salary desc
+limit 1;
+
+/* Which current department manager has the highest salary?
+ +------------+-----------+--------+-----------+
+| first_name | last_name | salary | dept_name |
++------------+-----------+--------+-----------+
+| Vishwani   | Minakawa  | 106491 | Marketing | */
+
+select first_name, last_name, salary, dept_name
+from employees
+join dept_manager using(emp_no)
+join salaries using(emp_no)
+join departments using(dept_no)
+where salaries.to_date > curdate()
+and dept_manager.to_date > curdate()
+order by salary desc
+limit 1;
+
+
+/* Bonus Find the names of all current employees, their department name, and their current manager's name.
+240,124 Rows
+
+Employee Name | Department Name  |  Manager Name
+--------------|------------------|-----------------
+ Huan Lortz   | Customer Service | Yuchang Weedman */
